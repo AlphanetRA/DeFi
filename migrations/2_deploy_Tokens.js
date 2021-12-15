5a68ee74015f50706d954a7e4e9ba5bdd3101f04
AlphanetRA <alphanetra0321@gmail.com>
2021-12-16 03:38:40 +0900

    create FarmToken smart contract

--- migrations/2_deploy_Tokens.js
+++ migrations/2_deploy_Tokens.js
@@ -1,6 +1,12 @@
 const MyToken = artifacts.require("MyToken")
+const FarmToken = artifacts.require("FarmToken")
 
 module.exports = async function (deployer, networks, acoounts) {
+    // deploy MyToken
     await deployer.deploy(MyToken)
     const myToken = await MyToken.deployed()
+
+    // deploy FarmToken
+    await deployer.deploy(FarmToken, myToken.address)
+    const farmToken = await FarmToken.deployed()
 }
